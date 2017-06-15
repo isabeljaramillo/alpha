dd79bf72dd4599bd25e53ba07d53178c9845076a
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-15 00:08:42 -0500

    Add table to list study rooms

--- room/db/migrate/20170614051443_create_reservations.rb
+++ room/db/migrate/20170614051443_create_reservations.rb
@@ -2,6 +2,7 @@ class CreateReservations < ActiveRecord::Migration[5.1]
   def change
     create_table :reservations do |t|
 
+      t.string :study_room
       t.time :time_in
       t.time :time_out
       t.datetime :reserve_date
