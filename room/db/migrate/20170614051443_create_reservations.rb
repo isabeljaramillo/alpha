338943a8bce650a981a5b375663b7620dfcbb421
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-15 00:04:53 -0500

    created more tables in db migration files

--- room/db/migrate/20170614051443_create_reservations.rb
+++ room/db/migrate/20170614051443_create_reservations.rb
@@ -2,6 +2,11 @@ class CreateReservations < ActiveRecord::Migration[5.1]
   def change
     create_table :reservations do |t|
 
+      t.time :time_in
+      t.time :time_out
+      t.datetime :reserve_date
+
+
       t.timestamps
     end
   end
--- room/db/migrate/20170614051710_create_study_rooms.rb
+++ room/db/migrate/20170614051710_create_study_rooms.rb
@@ -1,6 +1,9 @@
 class CreateStudyRooms < ActiveRecord::Migration[5.1]
   def change
     create_table :study_rooms do |t|
+      t.string :room_a
+      t.string :room_b
+      t.string :room_c
 
       t.timestamps
     end
