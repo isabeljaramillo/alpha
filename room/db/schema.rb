fa9a67b98adff5181422bf57ca0f0034534cd90f
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-15 22:35:32 -0500

    Run db:migration to add the FK to reservation
      $ rails db:migrate

--- room/db/schema.rb
+++ room/db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614051710) do
+ActiveRecord::Schema.define(version: 20170616025704) do
 
   create_table "reservations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "student_id"
+    t.index ["student_id"], name: "index_reservations_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
