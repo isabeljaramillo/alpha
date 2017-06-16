1d2b53019e8647e01a7afa5a77c8b4930fb7c792
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-15 22:11:47 -0500

    Spell 'reference' right

--- room/db/migrate/20170616025704_add_student_reference_to_reservation.rb
+++ room/db/migrate/20170616025704_add_student_reference_to_reservation.rb
@@ -1,5 +1,5 @@
 class AddStudentReferenceToReservation < ActiveRecord::Migration[5.1]
   def change
-    add_refernce :reservations, :student, foreign_key: true
+    add_reference :reservations, :student, foreign_key: true
   end
 end
